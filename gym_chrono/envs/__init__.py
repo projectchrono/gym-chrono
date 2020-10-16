beee019ed865cbab4fe093f063ff21462ddbbe5f
Benatti1991 <simone.benatti@studenti.unipr.it>
2020-10-16 15:10:09 +0200

    Delete v4 of Gator

--- gym_chrono/envs/__init__.py
+++ gym_chrono/envs/__init__.py
@@ -13,7 +13,6 @@ from gym_chrono.envs.off_road import off_road
 from gym_chrono.envs.Gator.off_road_gator_v1 import off_road_gator_v1
 from gym_chrono.envs.Gator.off_road_gator_v2 import off_road_gator_v2
 from gym_chrono.envs.Gator.off_road_gator_v3 import off_road_gator_v3
-from gym_chrono.envs.Gator.off_road_gator_v4 import off_road_gator_v4
 from gym_chrono.envs.Gator.off_road_gator import off_road_gator
 from gym_chrono.envs.GVSETS_env import GVSETS_env
 from gym_chrono.envs.robot_learning.robot_learning import robot_learning
