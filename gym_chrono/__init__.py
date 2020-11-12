6bbafa89f0edfaf2dca74f978701c5c9c143f0ed
Benatti1991 <simone.benatti@studenti.unipr.it>
2020-11-12 13:04:50 +0100

    Added driving environments, reorganized the whole repo

--- gym_chrono/__init__.py
+++ gym_chrono/__init__.py
@@ -13,17 +13,11 @@ register(
 register(
     id='chrono_hexapod-v0',
     entry_point='gym_chrono.envs:ChronoHexapod'
-    #timestep_limit=1000,
-    #reward_threshold=1.0,
-    #nondeterministic = True,
 )
 
 register(
     id='ChronoRacer3Reach-v0',
     entry_point='gym_chrono.envs:ChronoComauR3'
-    #timestep_limit=1000,
-    #reward_threshold=10.0,
-    #nondeterministic = True,
 )
 
 register(
@@ -47,13 +41,8 @@ register(
 )
 
 register(
-    id='gvsets-v0',
-    entry_point='gym_chrono.envs:GVSETS_env'
-)
-
-register(
-    id='off_road_gator-v1',
-    entry_point='gym_chrono.envs:off_road_gator_v1'
+    id='convoy-v0',
+    entry_point='gym_chrono.envs:convoy'
 )
 
 register(
@@ -61,41 +50,7 @@ register(
     entry_point='gym_chrono.envs:off_road_gator'
 )
 register(
-    id='off_road_gator-v2',
-    entry_point='gym_chrono.envs:off_road_gator_v2'
-)
-register(
     id='off_road_gator-v3',
     entry_point='gym_chrono.envs:off_road_gator_v3'
 )
-register(
-    id='robot_learning-v0',
-    entry_point='gym_chrono.envs:robot_learning'
-    #timestep_limit=1000,
-    #reward_threshold=10.0,
-    #nondeterministic = True,
-)
 
-register(
-    id='icra_tracked_leader-v0',
-    entry_point='gym_chrono.envs:icra_tracked_leader'
-)
-
-register(
-    id='icra_wheeled_leader-v0',
-    entry_point='gym_chrono.envs:icra_wheeled_leader'
-)
-register(
-    id='icra_gator-v0',
-    entry_point='gym_chrono.envs:icra_gator'
-)
-
-register(
-    id='icra_wheeled_follower-v0',
-    entry_point='gym_chrono.envs:icra_wheeled_follower'
-)
-
-register(
-    id='icra_tracked_follower-v0',
-    entry_point='gym_chrono.envs:icra_tracked_follower'
-)
