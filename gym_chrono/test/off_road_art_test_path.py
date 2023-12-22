ece07153167da5ca516b033e601095deebbc125c
Stefan Caldararu <stefancaldararu@gmail.com>
2023-12-22 23:53:14 +0000

    write off_road_art_path files

---
+++ gym_chrono/test/off_road_art_test_path.py
@@ -0,0 +1,41 @@
+import gymnasium as gym
+from gym_chrono.envs.wheeled.off_road_art_path import off_road_art
+from stable_baselines3.common.env_checker import check_env
+
+render = True
+additional_render = True
+if __name__ == '__main__':
+
+    # Add the agent POV as a render mode
+    if additional_render:
+        env = off_road_art(additional_render_mode='agent_pov')
+    else:
+        env = off_road_art()
+    # check_env(env)
+    # Set the mode to render for nice viz
+    env.set_nice_vehicle_mesh()
+    obs, _ = env.reset()
+    if render:
+        env.render('follow')
+
+    print(env.observation_space)
+    print(env.action_space)
+    print(env.action_space.sample())
+
+    # Hardcoded best agent: always go left!
+    n_steps = 1000000
+    for step in range(n_steps):
+        print(f"Step {step + 1}")
+        if (step < 100):
+            obs, reward, terminated, truncated, info = env.step([0.0, 0.7])
+        elif (step > 100):
+            obs, reward, terminated, truncated, info = env.step(
+                [0., 0.7, 0.])
+        print(obs)
+        print("Terminated=", terminated, "Truncated=", truncated)
+        done = terminated or truncated
+        if render:
+            env.render('follow')
+        if done:
+            print("reward=", reward)
+            break
---
+++ gym_chrono/train/off_road_art_train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+folder_path="/sbel/Documents/gym-chrono/gym_chrono/train/art_ppo_checkpoints_lidar"
+prefix="ppo_checkpoint"
+suffix=".zip"
+
+for ((i=0;i<100;i++)); do
+    # Change to the folder directory
+    cd "$folder_path" || exit
+
+    # Get the file with the largest number extension
+    latest_file=$(ls -1v $prefix*$suffix | tail -n 1)
+
+    if [[ $latest_file =~ $prefix([0-9]+)$suffix ]]; then
+        save_point=${BASH_REMATCH[1]}
+    fi
+
+    cd ../ || exit
+    python off_road_art_train_lidar.py $save_point
+done
