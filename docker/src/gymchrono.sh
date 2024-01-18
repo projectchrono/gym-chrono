fe940f84c6656b1caed7fae10e4afaadfb1b77be
Stefan Caldararu <stefancaldararu@gmail.com>
2024-01-18 16:55:27 -0600

    add gdown for data files

--- docker/src/gymchrono.sh
+++ docker/src/gymchrono.sh
@@ -2,5 +2,5 @@
 mkdir -p $HOME/Desktop
 cd $HOME/Desktop
 git clone https://github.com/projectchrono/gym-chrono.git
-# cd gym-chrono/gym_chrono/envs
-# gdown --no-cookies --folder https://drive.google.com/drive/folders/1g1X_GtMMgykAy2cVBMHnFnk99EL5QCin?usp=drive_link
+cd gym-chrono/gym_chrono/envs
+gdown --folder 1u4nwAlpPXtgkSJeBLlSM9B_utEoUIY41
